3cca62ed67d62a56fa522e75188269b4e474eb70
RafalDmoszynski <rafal.dmoszynski@gmail.com>
2023-12-03 19:40:03 +0100

    Dzień czwarty - zagnieżdżony if.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool woman = true;
+var age = 29;
+
+if (woman == true)
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33.");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat.");
+    }
+    else
+    {
+        Console.WriteLine("Twoja płęć to kobieta, pozostałe dane niezgodne.");
+    }
+}
+else
+{
+    if (!woman && age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna.");
+    }
+    else
+    {
+        Console.WriteLine("Error - wprowadź nowe dane !");
+    }
+}
+
